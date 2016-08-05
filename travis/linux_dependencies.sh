6e3da7d8f462cdde83a3136be6d77662e02146bd
Ethiy <oussama.ennafii@polytechnique.org>
2016-08-05 15:51:34 +0200

    Rearrange travis

---
+++ travis/linux_dependencies.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env sh
+
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -qq update
+sudo apt-get install -y build-essential g++-5 software-properties-common libboost-all-dev
+export CXX="g++-5"
+sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+sudo apt-get -qq update
+sudo apt-get install -y cmake
+sudo apt-get install -y libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev
+mkdir 3rdParty && cd 3rdParty
+git clone https://github.com/opencv/opencv.git 
+mkdir opencv/build && cd opencv/build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make -j4
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+cd ../../..
+pwd
---
+++ travis/osx_dependencies.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env sh
+
+brew update
+brew outdated
+brew upgrade
+brew tap homebrew/science
+brew install opencv
+pwd
